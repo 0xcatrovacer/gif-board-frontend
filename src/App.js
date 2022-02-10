d7e1a458ced92969b5d2245bccd5ed77622d252a
Swarnab Garang <swarnabgarang@gmail.com>
2022-02-11 01:34:20 +0530

    added rendering gifs in wallet connected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import twitterLogo from "./assets/twitter-logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
+const TEST_GIFS = [
+    "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
+    "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
+    "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
+    "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
+];
+
 // Constants
 const TWITTER_HANDLE = "0xcatrovacer";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
@@ -54,6 +61,28 @@ const App = () => {
         </button>
     );
 
+    const renderConnectedContainer = () => (
+        <div className="connected-container">
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                }}
+            >
+                <input type="text" placeholder="Enter gif link!" />
+                <button type="submit" className="cta-button submit-gif-button">
+                    Submit
+                </button>
+            </form>
+            <div className="gif-grid">
+                {TEST_GIFS.map((gif) => (
+                    <div className="gif-item" key={gif}>
+                        <img src={gif} alt={gif} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+
     useEffect(() => {
         const onLoad = async () => {
             await checkIfWalletIsConnected();
@@ -71,6 +100,7 @@ const App = () => {
                         View your GIF collection in the metaverse ✨
                     </p>
                     {!walletAddress && renderNotConnectedContainer()}
+                    {walletAddress && renderConnectedContainer()}
                 </div>
                 <div className="footer-container">
                     <img
